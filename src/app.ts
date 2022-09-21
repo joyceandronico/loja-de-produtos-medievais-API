fccbaa9e46296a092f947552dbc0813469ddae80
Joyce <joyceandronico@msn.com>
2022-09-21 13:48:11 -0300

    fix: corrige req 4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
+import orderRoutes from './routes/orders.routes';
 import productsRoutes from './routes/product.routes';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productsRoutes);
+app.use('/orders', orderRoutes);
 
 app.use(errorMiddleware);
 
