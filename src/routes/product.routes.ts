9ba662c1b5a287bd336f66e37cd9ed4bf56f2bfc
joyceandronicco <joyceandronico@msn.com>
2022-09-22 01:55:10 -0300

    feat: :sparkles: validações de produtos - req 6

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controllers/productController';
+import ProductsValidation from '../middlewares/productsValidation.middleware';
 
 const productsRoutes = Router();
 
 const productController = new ProductController();
+const validation = new ProductsValidation();
 
-productsRoutes.post('/', productController.createProduct);
+productsRoutes.post(
+  '/',
+  validation.nameValidation,
+  validation.amountValidation,
+  productController.createProduct,
+);
 productsRoutes.get('/', productController.getAllProducts);
 
 export default productsRoutes;
