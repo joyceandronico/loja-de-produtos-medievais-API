63407c3b4e71a125102811025805436ef528f329
joyceandronicco <joyceandronico@msn.com>
2022-09-22 02:21:50 -0300

    feat: :sparkles: validações de pessoas usuárias - req 7

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import UserController from '../controllers/userController';
+import UserValidations from '../middlewares/userValidation.middleware';
 
 const userRoutes = Router();
 
 const userController = new UserController();
+const validations = new UserValidations();
 
-userRoutes.post('/', userController.createNewUser);
+userRoutes.post(
+  '/',
+  validations.usernameValidation,
+  validations.classeValidation,
+  validations.levelValidation,
+  validations.passwordValidation,
+  userController.createNewUser,
+);
 
 export default userRoutes;
